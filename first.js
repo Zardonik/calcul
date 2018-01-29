23b795625b27bfbb2d55ec73b293582adfea12b4
Zardonik <infinityspeding@gmail.com>
2018-01-29 13:29:01 +0300

    create calcul

---
+++ first.js
@@ -0,0 +1,16 @@
+var firstNumber;
+var secondNumber;
+
+firstNumber = Number(prompt("input first Number"));
+secondNumber = Number(prompt("input second Number"));
+
+var A = firstNumber + secondNumber;
+var B = firstNumber - secondNumber;
+var C = firstNumber / secondNumber;
+var D = firstNumber * secondNumber;
+
+
+document.write("Sum is" + A + "<br>");
+document.write("subtraction is" + B + "<br>");
+document.write("division is" + C + "<br>");
+document.write("multiplication is" + D);
